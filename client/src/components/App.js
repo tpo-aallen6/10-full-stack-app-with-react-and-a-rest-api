a0ced540ec59b9248f83f8079d29ca6f93c59588
tpo-ebrito <tpo-ebrito@users.noreply.github.com>
2021-04-13 12:02:57 -0400

    updated components

---
+++ client/src/components/App.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import Courses from "./Courses";
+import CourseDetail from "./CourseDetail";
+
+const App = () => {
+
+  return (
+    <>
+      <CourseDetail />
+    </>
+  );
+};
+
+export default App;
