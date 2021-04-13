413e616f5200d2ad57ecc21b00139ce6df07006d
tpo-ebrito <tpo-ebrito@users.noreply.github.com>
2021-04-13 14:55:09 -0400

    working courses component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,7 +7,7 @@ const App = () => {
   return (
     <>
       <Header />
-      <UpdateCourse />
+      <Courses />
     </>
   );
 };
--- client/src/components/Courses.js
+++ client/src/components/Courses.js
@@ -9,34 +9,36 @@ const Courses = () => {
       .then((json) => setCourses(json.courses));
   }, []);
 
+  const renderedCourses = courses.map((course) => {
+    return (
+      <a
+        className="course--module course--link"
+        href="course-detail.html"
+        key={course.id}
+      >
+        <h2 className="course--label">Course</h2>
+        <h3 className="course--title">{course.title}</h3>
+      </a>
+    );
+  });
+
   return (
     <>
       <main>
-        <div class="wrap main--grid">
-          <a class="course--module course--link" href="course-detail.html">
-            <h2 class="course--label">Course</h2>
-            <h3 class="course--title">Build a Basic Bookcase</h3>
-          </a>
-          <a class="course--module course--link" href="course-detail.html">
-            <h2 class="course--label">Course</h2>
-            <h3 class="course--title">Learn How to Program</h3>
-          </a>
-          <a class="course--module course--link" href="course-detail.html">
-            <h2 class="course--label">Course</h2>
-            <h3 class="course--title">Learn How to Test Programs</h3>
-          </a>
+        <div className="wrap main--grid">
+          {renderedCourses}
           <a
-            class="course--module course--add--module"
+            className="course--module course--add--module"
             href="create-course.html"
           >
-            <span class="course--add--title">
+            <span className="course--add--title">
               <svg
                 version="1.1"
                 xmlns="http://www.w3.org/2000/svg"
                 x="0px"
                 y="0px"
                 viewBox="0 0 13 13"
-                class="add"
+                className="add"
               >
                 <polygon points="7,6 7,0 6,0 6,6 0,6 0,7 6,7 6,13 7,13 7,7 13,7 13,6 " />
               </svg>
