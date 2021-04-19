5f527fc97ad8edf15cf46272be36e91a5e819acc
tpo-ebrito <tpo-ebrito@users.noreply.github.com>
2021-04-19 13:06:07 -0400

    Update Header.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,28 +2,33 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default class Header extends React.PureComponent {
-  render() {
-    const { context } = this.props;
-    const authUser = context.authenticatedUser;
+  render () {
+    const { context } = this.props
+    const authUser = context.authenticatedUser
 
     return (
-      <div class="wrap header--flex">
-        <h1 class="header--logo"><Link to='/'>Courses</Link></h1>
-        <nav>
-          {authUser
-            ? <React.Fragment>
-            <span>Welcome, {authUser.user[0].firstName}!</span>
-            {console.log(authUser.user[0].firstName)}
-            <Link to="/signout"> Sign Out</Link>
-            </React.Fragment>
-            :
-            <React.Fragment>
-            <Link className="header--signedin" to="/signup">Sign Up</Link>
-            <Link className="header--signedout" to="/signin">Sign In</Link>
-            </React.Fragment>
-          }
-        </nav>
-      </div>
-    );
+      <header>
+        <div className='wrap header--flex'>
+          <h1 className='header--logo'><Link to='/'> Courses </Link></h1>
+          <nav>
+            {authUser
+               ? <React.Fragment>
+                   <ul className='header--signedin'></ul>
+                   <span>Welcome, {authUser.user[0].firstName}!</span>
+                   {console.log(authUser.user[0].firstName)}
+                   <Link to='/signout'> Sign Out
+                   </Link>
+                 </React.Fragment>
+               :
+               <React.Fragment>
+                 <Link className='header--signedin' to='/signup'> Sign Up
+                 </Link>
+                 <Link className='header--signedout' to='/signin'> Sign In
+                 </Link>
+               </React.Fragment>}
+          </nav>
+        </div>
+      </header>
+    )
   }
-};
+}
