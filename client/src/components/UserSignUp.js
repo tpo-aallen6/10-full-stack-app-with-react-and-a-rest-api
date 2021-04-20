d87a2d69db003a8b8529a720102fa226f9d81ab4
Adam Allen <aallen6@travelers.com>
2021-04-20 11:17:32 -0400

    fix indentation and spacing

--- client/src/components/UserSignUp.js
+++ client/src/components/UserSignUp.js
@@ -23,97 +23,97 @@ export default class UserSignUp extends Component {
     } = this.state;
 
     return (
-        <div class='form--centered'>
-          <h2>Sign Up</h2>
-          <Form 
-          cancel={this.cancel}
-          errors={errors}
-          submit={this.submit}
-          submitButtonText="Sign Up"
-          elements={() => (
-            <React.Fragment>
-              <label for='firstName'>First Name</label>
-              <input id='firstName' name='firstName' type='text' value={firstName} onChange={this.change} />
-              <label for='lastName'>Last Name</label>
-              <input id='lastName' name='lastName' type='text' value={lastName} onChange={this.change} />
-              <label for='emailAddress'>Email Address</label>
-              <input
-                id='emailAddress'
-                name='emailAddress'
-                type='email'
-                value={emailAddress}
-                onChange={this.change}
-              />
-              <label for='password'>Password</label>
-              <input id='password' name='password' type='password' value={password} onChange={this.change}  />
-              <label for='confirmPassword'>Confirm Password</label>
-              <input
-                id='confirmPassword'
-                name='confirmPassword'
-                type='password'
-                value={confirmPassword}
-                onChange={this.change}
-              />
-            </React.Fragment>
-          )} />
-          <p>
-            Already have a user account? Click here to{' '}
-            <Link to='/signin'>Sign In</Link>!
-          </p>
-        </div>
-  )
-}
-
-change = (event) => {
-  const name = event.target.name;
-  const value = event.target.value;
-
-  this.setState(() => {
-    return {
-      [name]: value
-    };
-  });
-}
-
-submit = () => {
-  const { context } = this.props;
-  const {
-    firstName,
-    lastName,
-    emailAddress,
-    password,
-  } = this.state;
-
-  const user = {
-    firstName,
-    lastName,
-    emailAddress,
-    password,
+      <div class='form--centered'>
+        <h2>Sign Up</h2>
+        <Form 
+        cancel={this.cancel}
+        errors={errors}
+        submit={this.submit}
+        submitButtonText="Sign Up"
+        elements={() => (
+          <React.Fragment>
+            <label for='firstName'>First Name</label>
+            <input id='firstName' name='firstName' type='text' value={firstName} onChange={this.change} />
+            <label for='lastName'>Last Name</label>
+            <input id='lastName' name='lastName' type='text' value={lastName} onChange={this.change} />
+            <label for='emailAddress'>Email Address</label>
+            <input
+              id='emailAddress'
+              name='emailAddress'
+              type='email'
+              value={emailAddress}
+              onChange={this.change}
+            />
+            <label for='password'>Password</label>
+            <input id='password' name='password' type='password' value={password} onChange={this.change}  />
+            <label for='confirmPassword'>Confirm Password</label>
+            <input
+              id='confirmPassword'
+              name='confirmPassword'
+              type='password'
+              value={confirmPassword}
+              onChange={this.change}
+            />
+          </React.Fragment>
+        )} />
+        <p>
+          Already have a user account? Click here to{' '}
+          <Link to='/signin'>Sign In</Link>!
+        </p>
+      </div>
+    )
   }
 
-   console.log(context)
-   console.log(this.props)
-   console.log(this.state)
+  change = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
 
-  context.data.createUser(user)
-  .then(errors => {
-    if (errors.length) {
-      this.setState({ errors });
-    } else {
-      context.actions.signIn(emailAddress, password)
-      .then(() => {
-        this.props.history.push('/authenticated')
-      })
+    this.setState(() => {
+      return {
+        [name]: value
+      };
+    });
+  }
+
+  submit = () => {
+    const { context } = this.props;
+    const {
+      firstName,
+      lastName,
+      emailAddress,
+      password,
+    } = this.state;
+
+    const user = {
+      firstName,
+      lastName,
+      emailAddress,
+      password,
     }
-  })
-  .catch( err => {
-    console.log(err);
-    this.props.history.push('/error');
-  })
-}
 
-cancel = () => {
-  this.props.history.push('/');
-}
+    console.log(context)
+    console.log(this.props)
+    console.log(this.state)
+
+    context.data.createUser(user)
+    .then(errors => {
+      if (errors.length) {
+        this.setState({ errors });
+      } else {
+        context.actions.signIn(emailAddress, password)
+        .then(() => {
+          this.props.history.push('/authenticated')
+        })
+      }
+    })
+    .catch( err => {
+      console.log(err);
+      this.props.history.push('/error');
+    })
+  }
+
+  cancel = () => {
+    this.props.history.push('/');
+  }
 }
 
